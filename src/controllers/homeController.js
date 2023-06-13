396ae2207f1d771c11e5c3efee4662eee1168fd0
Eric Neves <ericnevesrodrigues@gmail.com>
2023-06-13 15:37:23 -0400

    Add pagination

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,17 +7,32 @@ const products = new Product({
 
 exports.index = async (req, res) => {
   try {
-    const data = await products.fetchAll()
+    const dataFromDB = await products.fetchAll()
+
+    const { initial, offset } = req.query
+
+    let initialFormatted = parseInt(initial)
+    let offsetFormatted = parseInt(offset)
+
+    let previous = initialFormatted > 0
+    
+    const dataPartial = dataFromDB.slice(initialFormatted, offsetFormatted)
+
+    if (dataPartial.length === 0) {
+      return res.status(301).redirect('/?initial=0&offset=8')
+    }
     
     res.render('home/home', {
       title: 'Home - Shop',
       path: '/',
-      data
+      previous,
+      data: dataPartial
     })
   } catch (err) {
     res.render('home/home', {
       title: 'Home - Shop',
       path: '/',
+      previous,
       data: []
     })
   }
