dc01dbbbc3ac7986507451084483d4558e409bae
Eric Neves <ericnevesrodrigues@gmail.com>
2023-06-14 17:17:48 -0400

    GenerateID test completed

--- src/lib/generateID.test.js
+++ src/lib/generateID.test.js
@@ -1,3 +1,17 @@
 const { expect } = require('chai')
 const sinon = require('sinon')
 
+const { pure } = require('./generateID')
+
+describe('Test generate ID', function() {
+  it('should generate a uuid', function() {
+    const dependenciesUUID = sinon.stub().callsFake(() => ({
+      random: 10,
+      date: 1000 * 3600 * 24
+    }))
+
+    const uuid = pure.uuid({ dependenciesUUID })
+
+    expect(uuid()).to.equal('337f9800')
+  })
+})
